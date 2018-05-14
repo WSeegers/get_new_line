83c9d70d0e24b67cad86612496bbb97f71bfa132
Werner Seegers <wseegers.mauws@gmail.com>
2018-05-14 03:13:00 +0200

    needs clean up function

--- get_next_line.c
+++ get_next_line.c
@@ -5,9 +5,125 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: wseegers <wseegers.mauws@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/05/10 19:20:55 by wseegers          #+#    #+#             */
-/*   Updated: 2018/05/10 19:20:58 by wseegers         ###   ########.fr       */
+/*   Created: 2018/05/13 22:23:20 by wseegers          #+#    #+#             */
+/*   Updated: 2018/05/14 03:09:02 by wseegers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include "libft.h"
+#include "get_next_line.h"
+#include <stdio.h>
 
+static t_file *ft_getfile(int fd, t_list *flist)
+{
+	t_file *file;
+
+	while (((t_file*)flist->content)->fd != fd && flist->next)
+		if (flist->next)
+			flist = flist->next;
+	if (((t_file*)flist->content)->fd != fd)
+	{
+		file = (t_file*)ft_memalloc(sizeof(t_file));
+		*file = newfile(fd);
+		flist->next = ft_lstnew(file, sizeof(t_file));
+		file = (t_file*)flist->next->content;
+	} 
+	else
+		file = (t_file*)flist->content;
+	return (file);
+}
+
+static char ft_getchar(t_file* file)
+{
+	int ret;
+	
+	if (file->fpos == file->bufsize)
+	{
+		ret = read(file->fd, file->buf, BUFF_SIZE);
+		if (ret <= 0)
+		{
+			file->flag = ret;
+			return (ret);
+		}
+		file->bufsize = ret;
+		file->fpos = 0;
+	}
+	++file->fpos;
+	return (file->buf[file->fpos - 1]);
+}
+
+static void ft_getline(t_file *file, char **line)
+{
+	char *buf;
+	char *ret;
+	size_t size;
+	int pos;
+
+	size = 64;
+	pos = 0;
+	ret = ft_strnew(size);
+	while ((ret[pos] = ft_getchar(file)) != '\n' && ret[pos] > 0)
+	{
+		pos++;
+		if (pos == size - 1)
+		{
+			buf = ft_strnew(size *= 2);
+			ft_strcpy(buf, ret);
+			ft_strdel(&ret);
+			ret = buf;
+		}
+	}
+	if (ret[pos] == '\n')
+		ret[pos] = '\0';
+	*line = ret;
+}
+
+int		get_next_line(int fd, char **line)
+{
+	static t_list	*flist;
+	t_file			*file;
+	int 			ret;
+	
+	if (fd < 0 || !line)
+		return (-1);
+	if (!flist)
+	{
+		file = (t_file*)ft_memalloc(sizeof(t_file));
+		*file = newfile(fd);
+		flist = ft_lstnew(file, sizeof(t_file));
+	}
+	file = ft_getfile(fd, flist);
+ 	ft_getline(file, line);
+	return (file->flag);
+}
+
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(void)
+{
+	char *line;
+	int fd1;
+	int fd2;
+	int ret;
+	
+	ret = get_next_line(-1, &line);
+	printf("err-> %d\n", ret);
+	ret = get_next_line(23, &line);
+	printf("err-> %d\n", ret);
+	ret = get_next_line(23, NULL);
+	printf("err-> %d\n", ret);
+	fd1 = open("test_text1.txt", O_RDONLY);
+	fd2 = open("test_text2.txt", O_RDONLY);
+	while (get_next_line(fd1, &line))
+	{
+		printf("%s\n", line);
+		ft_strdel(&line);
+		if (get_next_line(fd2, &line))
+			printf("%s\n", line);
+		ft_strdel(&line);
+	}
+	close(fd1);
+	return (0);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -5,9 +5,31 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: wseegers <wseegers.mauws@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/05/10 19:19:50 by wseegers          #+#    #+#             */
-/*   Updated: 2018/05/10 19:20:50 by wseegers         ###   ########.fr       */
+/*   Created: 2018/05/13 22:13:33 by wseegers          #+#    #+#             */
+/*   Updated: 2018/05/14 02:16:51 by wseegers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEW_LINE_H
+# define GET_NEW_LINE_H
 
+# include "libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+# define BUFF_SIZE 32
+
+typedef unsigned int t_fpos;
+
+typedef struct			s_file
+{
+	int fd;
+	char *buf;
+	size_t bufsize;
+	t_fpos fpos;
+	int flag;
+}						t_file;
+
+# define newfile(fd) (t_file){fd, ft_strnew(BUFF_SIZE), 0, 0, 1}
+
+#endif
